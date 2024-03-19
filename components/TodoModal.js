87311d08c5935890e7d3ab14174ce5567270a133
jess <jessiewq1@gmail.com>
2024-03-18 23:45:23 -0300

    fix: TodoModal

--- components/TodoModal.js
+++ components/TodoModal.js
@@ -1,12 +1,26 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native'
+import { AntDesign } from '@expo/vector-icons'
 
 export default class TodoModal extends React.Component {
+  state = {
+    name: this.props.list.name,
+    color: this.props.list.color,
+    todos: this.list.todos,
+  }
+
   render() {
+
+    console.log(todos)
     return (
-      <View style={styles.container}>
-        <Text>List Modal</Text>
-      </View>
+      < SafeAreaView style={styles.container} >
+        <TouchableOpacity
+          style={{ position: 'absolute', top: 64, right: 32, zIndex: 10 }}
+          onPress={this.props.closeModal}
+        >
+          <AntDesign name='close' size={24} color={colors.black} />
+        </TouchableOpacity>
+      </SafeAreaView >
     )
   }
 }
